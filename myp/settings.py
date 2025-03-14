09de7114a6a02e06d651ba6a708c16359f5d40fb
hazalkoom <mwhamd345@gmail.com>
2025-03-14 20:02:18 +0200

    latest

--- myp/settings.py
+++ myp/settings.py
@@ -129,8 +129,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
-EMAIL_HOST = "smtp.gmail.com"  # Use Gmail's SMTP server
-EMAIL_PORT = 587  # Port for TLS encryption
-EMAIL_USE_TLS = True  # Secure the connection
-EMAIL_HOST_USER = "mwhamd345@gmail.com"  # ⬅️ Replace with your Gmail address
-EMAIL_HOST_PASSWORD = "pcfu yeez mvkf czcs" #⬅️ Replace with your Gmail App Password
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_PORT = 587  
+EMAIL_USE_TLS = True 
+EMAIL_HOST_USER = "mwhamd345@gmail.com"  
+EMAIL_HOST_PASSWORD = "pcfu yeez mvkf czcs" 
